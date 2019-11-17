462ba779f60ed551d56ec046b1eb4d5cb825fa5d
Daniel Ashcroft <dashcroft4@gmail.com>
2019-11-17 10:24:46 +0000

    2 models are able to be communicated with, 2 are just being opened and tested

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ LOCAL_ADDRESS = "localhost"
 app = Flask(__name__)
 
 diabetes_model = load_model("diabetes_8255.h5")
+prostate_cancer_model = load_model("my_prostate_cancer_model.h5")
+breast_cancer_model = load_model("my_breast_cancer_model.h5")
+heart_model = load_model("my_heart_model.h5")
 
 @app.route("/")
 def doc():
@@ -43,6 +46,53 @@ def testArgs():
 def tfversion():
 	tfversion = tf.__version__
 	return tfversion
+
+@app.route("/heart")
+def heart():
+	single_x_test = [52,1,0,125,212,0,1,168,0,1,2,2,3]
+	bigRes = heart_model.predict(np.array([single_x_test]))
+	res = bigRes[0][0]
+	return str(res)
+
+@app.route("/breast_cancer")
+def breast_cancer():
+	single_x_test = [20.57,17.77,132.9,1326,0.08474]
+	bigRes = breast_cancer_model.predict(np.array([single_x_test]))
+	res = bigRes[0][0]
+	return str(res)
+
+
+@app.route("/prostate_cancer")
+def prostate_cancer():
+	try:
+
+		val0 = request.args.get("val0")
+		val1 = request.args.get("val1")
+		val2 = request.args.get("val2")
+		val3 = request.args.get("val3")
+		val4 = request.args.get("val4")
+		val5 = request.args.get("val5")
+		val6 = request.args.get("val6")
+		val7 = request.args.get("val7")
+		val8 = request.args.get("val8")
+		IsRound = request.args.get("round")
+
+		# prostate_cancer?val0=1&val1=23&val2=12&val3=151&val4=954&val5=0.143&val6=0.51&val7=0.2&val8=0.010  returns   0.7160286
+
+		# prostate_cancer?val0=1&val1=23&val2=12&val3=151&val4=954&val5=0.143&val6=0.51&val7=0.2&val8=0.010&round=1 returns 1
+
+		single_x_test = [int(val0), int(val1), int(val2), int(val3), int(val4), float(val5), float(val6), float(val7), float(val8)]
+		BigRes = prostate_cancer_model.predict(np.array([single_x_test]))
+		res = BigRes[0][0]
+
+		if IsRound == "1":
+			res = round(float(res))
+
+		return str(res)
+
+	except Exception as e:
+		return str(e)
+
 
 @app.route("/diabetes")
 def diabetes():
